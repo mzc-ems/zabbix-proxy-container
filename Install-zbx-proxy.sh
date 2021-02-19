ec22dfbafacbd3c0a84167de8ac402f8ab66f9ad
Park ManHee <parkmh@mz.co.kr>
2021-02-19 19:09:41 +0900

    옵션 case문 구현

--- Install-zbx-proxy.sh
+++ Install-zbx-proxy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# Create Date: 2021-02-17 (parkmh@mz.co.kr / ManHee Park)
+# Description: The zabbix proxy server auto installer.
 
 OPTND=1
+TYPE_BASE='./zabbix-proxy'
 
 # Error message
 function err_msg() { echo "$@" ;} >&2
@@ -28,26 +31,26 @@ function show_help() {
     err_msg "How to install for zabbix proxy server."
     err_msg
     err_msg "Usage:" 
-    err_msg "  $(basename "$0") [options] [TYPE <basic|build>]"
+    err_msg "  $(basename "$0") [-tns]"
     err_msg
     err_msg "Options:"
-    err_msg "  -n <name>          Specify a zabbix proxy server name"
-    err_msg "  -s <host|ip>       Specify the zabbix server hostname or ip address"
-    err_msg "  -t <basic|build>   Reference to Type section"
-    err_msg "  -h                 This help text"
+    err_msg "  -t <lastest|local>   Reference to Type section"
+    err_msg "  -n <name>            Specify a zabbix proxy server name"
+    err_msg "  -s <host|ip>         Specify the zabbix server hostname or ip address"
+    err_msg "  -h                   This help text"
     err_msg 
     err_msg "Type:"
-    err_msg "  basic              Specify a default type to install container"
-    err_msg "  build              Specify a custom build to install container"
-    err_msg "                     Include a dockerfile"
+    err_msg "  lastest              Specify a default type to install container"
+    err_msg "  local                Specify a custom build to install container"
+    err_msg "                       Include a dockerfile"
     exit 1
 }
 
 # Install the zabbix proxy server.
 function install_zbx_proxy() {
-    if [[ "$TYPE" == basic ]]; then
+    if [[ "$TYPE" == lastest ]]; then
         echo "docker-compose -f ./zabbix-proxy_latest/docker-compose.yml up -d"
-    elif [[ "$TYPE" == build ]]; then
+    elif [[ "$TYPE" == 'local' ]]; then
         echo "docker-compose -f ./zabbix-proxy_local/docker-compose.yml up -d --build"
     else
         echo "Please enter either $(color_msg yello basic) or $(color_msg yello build)."
@@ -55,13 +58,44 @@ function install_zbx_proxy() {
 }
 
 # Main
+if [[ -n /etc/system-release ]]; then
+
+elif [[ -n /etc/lsb-release ]]; then
+
+fi
+
 # Short options
-while getopts ":n:s:t:h" opt; do 
+while getopts ":t:n:s:h" opt; do 
     case $opt in
+        t)
+            if [[ -z "$OPTARG" ]] || [[ ! -n "$OPTARG" ]]; then
+                TYPE=lastest
+                echo " TYPE is default"
+            else
+                TYPE="$OPTARG"
+            fi 
+            if [[ "$TYPE" =~ lastest|local ]]; then
+                color_msg green "Start installing zabbix proxy server .....\n"
+                install_zbx_proxy
+                echo "-t arguments OK"
+            else
+                err_msg $(color_msg red "error: select lastest or local.")
+                echo "$OPTARG"
+                exit 1
+            fi
+            ;;
         n)
             ZBX_PROXY_NAME="$OPTARG"
-            if [[ $ZBX_PROXY_NAME =~ ^[A-Za-z].+$ ]]; then
-                echo "-n arguments OK"         
+            if [[ "$ZBX_PROXY_NAME" =~ ^-t ]] || [[ "$ZBX_PROXY_NAME" =~ ^[A-Za-z].+$ ]]; then
+                echo "-n arguments OK"
+                 CNT=`grep -c 'ZBX_HOSTNAME' ${TYPE_BASE}_${TYPE}/.env_prx`
+                
+                if [[ "$CNT" -ne 0 ]]; then
+                    err_msg $(color_msg yello "error: check ZBX_HOSTNAME in the .env_prx files.")
+                    exit 1
+                else
+                    echo "ZBX_HOSTNAME=$ZBX_PROXY_NAME" >> ${TYPE_BASE}_${TYPE}/.env_prx
+                fi        
             else
                 err_msg "error: the first letter cann't digit or special character"
                 echo "$OPTARG"
@@ -70,27 +104,18 @@ while getopts ":n:s:t:h" opt; do
             ;;
         s)
             ZBX_SERVER="$OPTARG"
-            if [[ $ZBX_SERVER =~ ^-n ]] && [[ $ZBX_SERVER =~ ^[A-Za-z].+$ ]] || [[ $ZBX_SERVER =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9][{1,3}\.[0-9]{1,3}$ ]]; then
+            if [[ "$ZBX_SERVER" =~ ^-n ]] || [[ "$ZBX_SERVER" =~ ^[A-Za-z].+$ ]] || [[ "$ZBX_SERVER" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9][{1,3}\.[0-9]{1,3}$ ]]; then
                 echo "-s arguments OK"
+                CNT=`grep -c 'ZBX_SERVER_HOST' ${TYPE_BASE}_${TYPE}/.env_prx`
+                
+                if [[ "$CNT" -ne 0 ]]; then
+                    err_msg $(color_msg yello "error: check ZBX_SERVER in the .env_prx files.")
+                    exit 1
+                else
+                    echo "ZBX_SERVER_HOST=${ZBX_SERVER}" >> ${TYPE_BASE}_${TYPE}/.env_prx
+                fi
             else
                 err_msg $(color_msg red "error: check hostname or ip address.")
-                echo "$OPTARG"
-                exit 1
-            fi
-            ;;
-        t)
-            if [[ -z $OPTARG ]] || [[ ! -n $OPTARG ]]; then
-                TYPE=basic
-                echo " TYPE is default"
-            else
-                TYPE="$OPTARG"
-            fi 
-            if [[ $TYPE =~ basic|build ]]; then
-                color_msg green "Start installing zabbix proxy server .....\n"
-                install_zbx_proxy
-                echo "-t arguments OK"
-            else
-                err_msg $(color_msg red "error: select basic or build.")
                 echo "$OPTARG"
                 exit 1
             fi
@@ -111,9 +136,13 @@ while getopts ":n:s:t:h" opt; do
 done
 
 shift $(( OPTND - 1 ))
+
 echo "-n ARG is $ZBX_PROXY_NAME"
 echo "-s ARG is $ZBX_SERVER"
 echo "-t ARG is $TYPE"
+
+
+
 color_msg green "Completed installing zabbix proxy server .....\n"
 
 exit 0
